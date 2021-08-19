171d6221e0f4b69d6ff214dc5aa8347f583b2ae5
akbuluth <36998018+akbuluth@users.noreply.github.com>
2021-08-19 09:43:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import glob
+import datetime
+date = datetime.datetime.now()
+
+directory = sys.argv[1]
+all_files = glob.glob(directory+"/*.log")
+result = {}
+
+for each_file in all_files:
+    file = open(each_file)
+    all_lines = file.readlines()
+
+    for each_line in all_lines:
+        temp = each_line.split("|")
+        if result.get(temp[0]):
+            try:
+                result[temp[0]] += int(temp[1])
+            except:
+                continue
+        else:
+            try:
+                result[temp[0]] = int(temp[1])
+            except:
+                continue
+    file.close()
+
+sorted_result_list = sorted(result.items(), key=lambda x:x[1], reverse=True)
+result = dict(sorted_result_list[:250002])
+
+result.pop("acs.doping.com.tr")
+result.pop("a.root-servers.net")
+
+final = open("MILLENICOM-GIH-DNS_250K-"+date.strftime("%Y%m%d")+".txt", "w")
+
+for i in result:
+    final.write(i + "|" + str(result[i]) + "\n")
+
+final.close()
